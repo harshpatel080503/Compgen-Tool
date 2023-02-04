70613ad0b229a9fd221b546df5f42a7d0f37a7ff
harshpatel080503 <harshpatel080503@gmail.com>
2023-02-04 12:46:21 +0530

    finished project

---
+++ app/src/main/java/com/example/hackinfinity30/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hackinfinity30;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button b1 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b1 = new Intent(MainActivity.this , product_detail.class);
+                startActivity(b1);
+            }
+        });
+    }
+}
